9f341d88feebc10ef593ba2a0794135dc66be17b
stefanro2000 <stefanro2000@gmail.com>
2020-08-29 15:06:06 -0400

    Add files via upload

---
+++ ToGoOrders/ToGoOrders/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace ToGoOrders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.Send = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.customersDataSet = new ToGoOrders.CustomersDataSet();
+            this.customersDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(366, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Phone#:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(607, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter CC:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(595, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Previous Order:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Order:";
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(36, 224);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(378, 290);
+            this.listBox5.TabIndex = 9;
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(636, 542);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(171, 77);
+            this.Send.TabIndex = 10;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "Jon",
+            "Mary",
+            "Bob"});
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox1.Location = new System.Drawing.Point(75, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox2.Location = new System.Drawing.Point(420, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 20);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 77);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Add New Customer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(243, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 77);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Existing Customer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(666, 10);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(199, 20);
+            this.textBox3.TabIndex = 15;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(449, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(171, 77);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Change Customer Name";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(655, 74);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(171, 77);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Update Credit Card";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.DarkRed;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(859, 74);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(171, 77);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Delete Customer";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(598, 224);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(432, 290);
+            this.dataGridView1.TabIndex = 19;
+            // 
+            // customersDataSet
+            // 
+            this.customersDataSet.DataSetName = "CustomersDataSet";
+            this.customersDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // customersDataSetBindingSource
+            // 
+            this.customersDataSetBindingSource.DataSource = this.customersDataSet;
+            this.customersDataSetBindingSource.Position = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(1149, 643);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ToGoOrders";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private CustomersDataSet customersDataSet;
+        private System.Windows.Forms.BindingSource customersDataSetBindingSource;
+    }
+}
+
---
+++ ToGoOrders/ToGoOrders/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Data.SqlServerCe;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ToGoOrders
+{
+    public partial class Form1 : Form
+    {
+        public static string id, name, phone, creditCard;
+        DBAccess objDBAccess = new DBAccess();
+        DataTable dtUsers = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlCeConnection con = new SqlCeConnection(Properties.Settings.Default.CustomersConnectionString);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlCeCommand cmd = new SqlCeCommand("SELECT Phone FROM Test", con);
+            con.Open();
+            SqlCeDataReader dr = cmd.ExecuteReader();
+            AutoCompleteStringCollection myCollection = new AutoCompleteStringCollection();
+
+            while (dr.Read())
+            {
+                myCollection.Add(dr.GetString(0));
+            }
+            textBox2.AutoCompleteCustomSource = myCollection;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string custName = textBox1.Text;
+            string custPhone = textBox2.Text;
+            string custCC = textBox3.Text;
+            bool exist = false;
+
+            string query = "SELECT * FROM Users WHERE Phone = '" + custPhone + "'";
+            objDBAccess.readDatathroughAdapter(query, dtUsers);
+            if (dtUsers.Rows.Count == 1)
+            {
+                exist = true;
+            }
+            if (custName.Equals(""))
+            {
+                MessageBox.Show("Invalid customer name!");
+            }
+            else if (custPhone.Equals(""))
+            {
+                MessageBox.Show("Customer phone can't be blank!");
+            }
+            else if(exist == false)
+            {
+                SqlCommand insertCommand = new SqlCommand(" INSERT INTO Users(Name, Phone, CC) VALUES (@custName, @custPhone, @custCC)");
+
+                insertCommand.Parameters.AddWithValue("@custName", custName);
+                insertCommand.Parameters.AddWithValue("@custPhone", custPhone);
+                insertCommand.Parameters.AddWithValue("@custCC", custCC);
+                int row = objDBAccess.executeQuery(insertCommand);
+
+                if (row == 1)
+                {
+                    MessageBox.Show("Account Created Succesfully");
+                    Form1 form = new Form1();
+                    form.Show();
+                    //this.Hide();
+                    //HomePage home = new HomePage();
+                    //home.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Error occured. Try Again");
+                }
+            } 
+            else if(exist == true)
+            {
+                MessageBox.Show("Phone number already entered");
+
+            }
+
+        }
+
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+
+            //MessageBox.Show("Enter name");
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Verify that the pressed key isn't CTRL or any non-numeric digit
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // If you want, you can allow decimal (float) numbers
+            //if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            //{
+            //    e.Handled = true;
+            //}
+
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            string sVal = textBox2.Text;
+            if (!string.IsNullOrEmpty(sVal) && e.KeyCode != Keys.Back)
+            {
+                sVal = sVal.Replace("-", "");
+                string newSt = Regex.Replace(sVal, ". {3}", "$0-");
+                //newSt = Regex.Replace(sVal, ".{6}", "$0-");
+                textBox2.Text = newSt;
+                textBox2.SelectionStart = textBox2.Text.Length;
+            }
+        }
+
+
+        public static string formatPhoneNumber(string phoneNum, string phoneFormat)
+        {
+
+            if (phoneFormat == "")
+            {
+                // If phone format is empty, code will use default format (###) ###-####
+                phoneFormat = "(###) ###-####";
+            }
+
+            // First, remove everything except of numbers
+            Regex regexObj = new Regex(@"[^\d]");
+            phoneNum = regexObj.Replace(phoneNum, "");
+
+            // Second, format numbers to phone string
+            if (phoneNum.Length > 0)
+            {
+                phoneNum = Convert.ToInt64(phoneNum).ToString(phoneFormat);
+            }
+
+            return phoneNum;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //search for a customer and match it
+            //in case there is not customer with the phone# entered a message needs to be displayed e.g."no customer was found"
+            string userPhone = textBox2.Text;
+
+            if (userPhone.Equals(""))
+            {
+                MessageBox.Show("Phone field cannot be empty!");
+            }
+            else
+            {
+                string query = "SELECT * FROM Users WHERE Phone = '" + userPhone + "'";
+                objDBAccess.readDatathroughAdapter(query, dtUsers);
+                if (dtUsers.Rows.Count == 1)
+                {
+                    //the name field should give the option to store more names for the same ph#
+                    id = dtUsers.Rows[0]["ID"].ToString();
+                    name = dtUsers.Rows[0]["Name"].ToString();
+                    creditCard = dtUsers.Rows[0]["CC"].ToString();
+                    textBox1.Text = name;
+                    textBox3.Text = creditCard;
+                }
+                else if (dtUsers.Rows.Count == 0)
+                {
+                    MessageBox.Show("No customer was found");
+                    textBox2.Text = "";
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string newName = textBox1.Text;
+            string newPhone = textBox2.Text;
+            string newCreditCard = textBox3.Text;
+            if (newName.Equals("") && newPhone.Equals(""))
+            {
+                MessageBox.Show("No customer was entered");
+            }
+            else if (newPhone.Equals(""))
+            {
+                MessageBox.Show("Phone field can not be blank");
+            }
+            else if (newName.Equals(""))
+            {
+                MessageBox.Show("Name field can not be blank");
+            }
+            else
+            {
+                string query = "Update Users SET Name = '" + @newName + "',Phone = '" + @newPhone + "' WHERE ID = '" + Form1.id + "' ";
+                SqlCommand updateCommand = new SqlCommand(query);
+
+                updateCommand.Parameters.AddWithValue("@newName", newName);
+                updateCommand.Parameters.AddWithValue("@newPhone", newPhone);
+
+                int row = objDBAccess.executeQuery(updateCommand);
+
+                if (row == 1)
+                {
+                    MessageBox.Show("Account Modified Succesfully");
+                }
+                else
+                {
+                    MessageBox.Show("Error occured. Try Again");
+                }
+
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Are you sure?", "Delete user", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if(dialog == DialogResult.Yes){
+                string query = "DELETE FROM Users WHERE ID   = '" + Form1.id + "'";
+                SqlCommand deleteCommand = new SqlCommand(query);
+                int row = objDBAccess.executeQuery(deleteCommand);
+
+                if (row == 1)
+                {
+                    MessageBox.Show("Customer account deleted succesfully");
+                }
+                else
+                {
+                    MessageBox.Show("Error occured. Try Again");
+                }
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" || textBox2.Text != "")
+            {
+                CreditCardForm CCForm = new CreditCardForm();
+                CCForm.Show();
+            }
+            else if (textBox1.Text == "")
+            {
+                MessageBox.Show("Name can not be empty");
+            }
+            else if (textBox2.Text == "")
+            {
+                MessageBox.Show("Phone can not be empty");
+            }
+        }
+
+    }
+}
